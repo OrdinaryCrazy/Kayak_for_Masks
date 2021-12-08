c5b588f60b370cc15d201a01a8596a99d7df7686
yyc <yyc@tamu.edu>
2021-12-08 13:56:10 -0600

    All Mask

--- KayakMask/masklink/spider_1206.py
+++ KayakMask/masklink/spider_1206.py
@@ -51,32 +51,62 @@ class Spider(object):
         req = requests.get(url, headers=headers)
         
         # modify each if/elif to abstract price -- Siqi
+
+        #Pod
         if url == 'https://www.podsupplies.com/products/kids-mask':
             url = 'https://www.podsupplies.com/products/kids-mask'
             s_div_name = 'price__sale'
-            o_div_name = 'price__regular'
+            # o_div_name = 'price__regular'
             sale_span_name = 'price-item price-item--sale'
-            orig_span_name = 'price-item price-item--regular'
+            # orig_span_name = 'price-item price-item--regular'
             s_div = self.get_price_div(url, s_div_name)
             sale_price = self.get_price(s_div, sale_span_name,'span').strip()
 
+        #Wayre
         elif url == 'https://www.shopwayre.com/collections/all/products/kids-high-tech-washable-mask':
             # There are many prices in this page, I choose only one from them -- Siqi
-            soup = BeautifulSoup(req.text, 'html.parser')
-            texts = soup.find('meta', property='product:price:amount')
-            sale_price = '$'+texts["content"]
-
-        else:
-            # THis is usless
-            url = 'https://www.podsupplies.com/products/kids-mask'
-            s_div_name = 'price__sale'
-            o_div_name = 'price__regular'
-            sale_span_name = 'price-item price-item--sale'
-            orig_span_name = 'price-item price-item--regular'
+            # soup = BeautifulSoup(req.text, 'html.parser')
+            # texts = soup.find('meta', property='product:price:amount')
+            # sale_price = '$'+texts["content"]
+            url = 'https://www.shopwayre.com/collections/all/products/kids-high-tech-washable-mask'
+            s_div_name = "ProductMeta__PriceList Heading"  
+            sale_span_name = "ProductMeta__Price Price Price--highlight Text--subdued u-h4"
             s_div = self.get_price_div(url, s_div_name)
             sale_price = self.get_price(s_div, sale_span_name,'span').strip()
-            # o_div = self.get_price_div(url, o_div_name)
-            # orig_price = self.get_price(o_div, orig_span_name, 'span').strip()
+
+
+        # Happy Mask 
+        elif url == 'https://www.happymasks.com/collections/pro/products/black-pro':
+            url = 'https://www.happymasks.com/collections/pro/products/black-pro'
+            s_div_name = 'price-container'
+            sale_span_name = 'product-single__price'
+            s_div = self.get_price_div(url, s_div_name)
+            sale_price = self.get_price(s_div, sale_span_name,'span').strip()
+        
+        #Honeywell 
+        elif url == 'https://ppe.honeywell.com/collections/face-covers/products/honeywell-3d-knit-face-mask-light-grey-size-s-m-4-pk':
+            url = 'https://ppe.honeywell.com/collections/face-covers/products/honeywell-3d-knit-face-mask-light-grey-size-s-m-4-pk'
+            s_div_name = 'product-price'
+            sale_span_name = 'money bfx-price'
+            s_div = self.get_price_div(url, s_div_name)
+            sale_price = self.get_price(s_div, sale_span_name,'span').strip()
+
+        #Caraa
+        elif url == 'https://caraasport.com/products/3-tailored-junior-masks?variant=39513097764944':
+            url = 'https://caraasport.com/products/3-tailored-junior-masks?variant=39513097764944'
+            s_div_name = 'price-flex'
+            sale_span_name = 'money'
+            s_div = self.get_price_div(url, s_div_name)
+            sale_price = self.get_price(s_div, sale_span_name,'span').strip()
+
+
+        #Flomask 
+        else:
+            url = 'https://flomask.com/collections/flo-mask-bundle-mask-12-filters/products/flo-mask-essential-bundle'
+            s_div_name = "price__regular"
+            sale_span_name = "price-item price-item--regular"
+            s_div = self.get_price_div(url, s_div_name)
+            sale_price = self.get_price(s_div, sale_span_name,'span').strip()
         print(sale_price)
         return sale_price
   
