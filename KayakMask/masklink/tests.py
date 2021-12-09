3ab2fcc8a22bdc4259d382e4af7d8580a9896a6d
KeigoUtdMa <54554309+KeigoUtdMa@users.noreply.github.com>
2021-12-09 02:55:21 -0600

    Unit Test
    Spider Test

--- KayakMask/masklink/tests.py
+++ KayakMask/masklink/tests.py
@@ -1,13 +1,17 @@
+from unittest.runner import TextTestResult
 from django import forms
+from django import test
 from django.forms.forms import Form
 from django.test import TestCase
 from django.db import models
+from requests.api import request
 from masklink.models import MaskInfo
 from django.urls.conf import _path,path
 from functools import partial
 from django.urls.resolvers import RoutePattern
 from masklink.views import MaskIndex, MaskSpider, MaskChoiceForm
 from .urls import urlpatterns
+from masklink.spider_1206 import Spider
 # Create your tests here.
 
 #Test cases for the mask list display
@@ -67,9 +71,43 @@ class availabilityTestCase(TestCase):
         index = MaskChoiceForm.form.mask_sheet.index
         mask_sheet = MaskChoiceForm.form.mask_sheet
         form = MaskChoiceForm.form.data.mask_sheet.drop(index[mask_sheet['avai'].data])
-
     def test_Ava(form):
         if form == "1":
             form['Availablity']=='No'
         elif form  == '0':
             form['Availablity']=='Yes'
+
+#div spider
+class divWebSpiderTestCase(TestCase):
+    def webSpiderSetup(self, req, Spider, url, requests, div_class_name, str, div_bf,texts):
+        req = requests.get(url)
+        texts = div_bf.find_all('div', class_=div_class_name)
+        str = Spider(texts)
+
+    def test_div(self):
+        if str == 'div':
+            return str(TextTestResult)
+
+#price spider
+class priceWebSpiderTestCase(TestCase):
+    def priceSpiderSetup(self, Spider, BeautifulSoup, div_bf, div_tag, class_name):
+        price_bf = BeautifulSoup(div_bf,features="lxml")
+        price = str(price_bf.find(div_tag, {'class' : class_name}).string)        
+
+    def test_price(price):
+        return price
+
+#abstract price (one example)
+class abstract_priceTestCase(TestCase):
+    def abstracrTestCase(self, url, header, req, requests, headers, s_div_name, sale_span_name, s_div):
+        if url == 'https://www.podsupplies.com/products/kids-mask':
+            req = requests.get(url, headers = headers)
+            s_div_name = 'price__sale'
+            sale_span_name = 'price-item price-item--sale'
+            sale_price = self.get_price(s_div, sale_span_name,'span').strip()
+        else:
+            return 0
+
+    def test_abs_price(sale_price):
+        print(sale_price)
+        return sale_price
