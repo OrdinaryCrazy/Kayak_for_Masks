13c8d31cd72dde90657af585261854ce614397d2
Zhang <zjt6791@tamu.edu>
2021-12-09 14:07:11 -0600

    deploy

--- KayakMask/masklink/views.py
+++ KayakMask/masklink/views.py
@@ -129,7 +129,7 @@ class MaskLinkSpider(object):
         # Multi-Brand Filter Ends
 
         if self.form['avai'].data == "1":
-            self.mask_sheet.drop(self.mask_sheet.index[self.mask_sheet['Availablity']=='All'], inplace=True)
+            self.mask_sheet.drop(self.mask_sheet.index[self.mask_sheet['Availablity']=='No'], inplace=True)
         #elif self.form['avai'].data == '0':
         #    self.mask_sheet.drop(self.mask_sheet.index[self.mask_sheet['Availablity']=='Yes'], inplace=True)
         # Filtering end
